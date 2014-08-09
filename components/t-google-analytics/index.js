85385165ea24678e7a52c02b52c1168030951c53
Kyle Finley <git@kylefinley.com>
2014-08-09 01:16:27 -0500

    components: updated t-google-analytics

--- components/t-google-analytics/index.js
+++ components/t-google-analytics/index.js
@@ -1,4 +1,4 @@
 module.exports = GoogleAnalytics;
 function GoogleAnalytics() {}
-GoogleAnalytics.prototype.name = 'google-analytics';
+GoogleAnalytics.prototype.name = 't-google-analytics';
 GoogleAnalytics.prototype.view = __dirname;
